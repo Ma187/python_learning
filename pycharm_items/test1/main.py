79b85be185bb6833b1688d78e28274caadc216aa
Ma187 <1870852337@qq.com>
2021-04-26 16:35:34 +0800

    second commit

---
+++ pycharm_items/test1/main.py
@@ -0,0 +1,65 @@
+import requests
+import sys
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
+# 执行API调用并存储响应
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print("Status code:", r.status_code)
+# 将API响应存储在一个变量中
+response_dict = r.json()
+# 处理结果
+# print(response_dict.keys())
+# print("Total repositories: ", response_dict['total_count'])
+
+#
+repo_dicts = response_dict['items']
+# print("Repositories returned:", len(repo_dicts))
+
+# repo_dict = repo_dicts[0]
+# print("\n Keys:", len(repo_dict))
+
+# for key in sorted(repo_dict.keys()):
+#     print(key)
+
+# print(repo_dict)
+
+# non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
+# print(x.translate(non_bmp_map))
+
+# names, stars = [], []
+# for repo_dict in repo_dicts:
+#     names.append(repo_dict['name'])
+#     stars.append(repo_dict['stargazers_count'])
+names, plot_dicts = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': str(repo_dict['description']),
+        'xlink': repo_dict['html_url']  # 使得点击图形可以访问链接
+    }
+    plot_dicts.append(plot_dict)
+
+my_style = LS('#333366', base_style=LCS)
+# chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most-Starred Python Projects on Git Hub'
+chart.x_labels = names
+chart.add('', plot_dicts)
+chart.render_to_file('stars_chart.svg')
+
+
+
+
